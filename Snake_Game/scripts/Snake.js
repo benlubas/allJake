7ad4bd71c7ecf4f482a4c47ba5e951ebb50b276f
benlubas <benmlubas@gmail.com>
2019-12-09 22:20:03 -0500

    settings, play again, styles bug fixes

--- Snake_Game/scripts/Snake.js
+++ Snake_Game/scripts/Snake.js
@@ -1,53 +1,51 @@
-function Snake(x, y, len, color){
-  this.x = x; 
-  this.y = y; 
+function Snake(x, y, len, color) {
+  this.x = x;
+  this.y = y;
   this.len = len;
   this.xDir = 1;
-  this.yDir = 0; 
-  this.fed = false; 
-  this.color = color; 
-  this.lock = false; 
-  this.inputQ = []; 
-
+  this.yDir = 0;
+  this.color = color;
+  this.moving = [1, 0];
+  this.growPer = gamerule.lengthGain;
+  this.growing = 0;
   this.positions = [];
 }
 
-Snake.prototype.move = function(){
-  if (k.w && (this.yDir != 1 || this.len == 1)){
-    this.yDir = -1; 
-    this.xDir = 0; 
-  } 
-  if (k.a && (this.xDir != 1 || this.len == 1)) {
-    this.xDir = -1; 
-    this.yDir = 0; 
-  }
-  if (k.s && (this.yDir != -1 || this.len == 1)) {
-    this.yDir = 1; 
+Snake.prototype.move = function() {
+  if (k.w && (this.moving[1] != 1 || this.len == 1)) {
+    this.yDir = -1;
     this.xDir = 0;
   }
-  if (k.d && (this.xDir != -1 || this.len == 1)) {
-    this.xDir = 1; 
+  if (k.a && (this.moving[0] != 1 || this.len == 1)) {
+    this.xDir = -1;
+    this.yDir = 0;
+  }
+  if (k.s && (this.moving[1] != -1 || this.len == 1)) {
+    this.yDir = 1;
+    this.xDir = 0;
+  }
+  if (k.d && (this.moving[0] != -1 || this.len == 1)) {
+    this.xDir = 1;
     this.yDir = 0;
   }
   // console.log(this.xDir);
   // console.log(this.yDir);
-}
-Snake.prototype.update = function(foodPos){
-  this.positions.push(new Point(this.x + this.xDir, this.y + this.yDir)); 
-  this.x += this.xDir; 
-  this.y += this.yDir; 
-  console.log(this.positions);
-  // if the head is on some food. 
-  if(arrIncludesPoint(this.positions, foodPos)){
-    this.fed = true; 
-    this.len++; 
-    foodExists = false; 
+};
+Snake.prototype.update = function(foodPos) {
+  this.positions.push(new Point(this.x + this.xDir, this.y + this.yDir));
+  this.x += this.xDir;
+  this.y += this.yDir;
+  this.moving = [this.xDir, this.yDir];
+  // console.log(this.positions);
+  // if the head is on some food.
+  if (this.positions.includesPoint(foodPos)) {
+    this.growing = this.growPer.random();
+    this.len++;
+    foodExists = false;
   }
 
-
-  if(!this.fed)
-    this.positions.splice(0, 1);
-  else{
-    this.fed = false; 
+  if (this.growing <= 0) this.positions.splice(0, 1);
+  else {
+    this.growing--;
   }
-}
+};
